85e65a8ed2800f42721e2c7e2f05ce9309c5abc8
변영수 <byunyoungsu>
2021-05-20 22:27:25 +0900

    window

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+var list = document.getElementById('list');
+
+button.addEventListener('click', clickButton);
+
+function clickButton(){
+  var temp = document.createElement('li');
+  temp.innerHTML = input.value;
+  list.appendChild(temp);
+}
